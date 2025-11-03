26c0d9d7f0c3a8d3333e00fc2cff6328e50f8f74
RenataSak <renasaku@madona.edu.lv>
2025-11-03 10:57:44 +0200

    Izlabots uzdevums1.py
    Atradu trīs kļūdas - paskaidrotas.

--- uzdevums1.py
+++ uzdevums1.py
@@ -2,7 +2,7 @@ import math
 # Ievade
 a = float(input("Ievadi a: "))
 b = float(input("Ievadi b: "))
-c = float(input("Ievadi c: ")
+c = float(input("Ievadi c: ")) #trūka iekavas
 
 # Diskriminants
 D = b**2 - 4*a*c
@@ -10,10 +10,10 @@ D = b**2 - 4*a*c
 # Sakņu aprēķins
 if D > 0:
     x1 = (-b + math.sqrt(D)) / (2*a)
-    x = (-b - math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a) # nepareizs mainīgā nosaukums
     print(f"D = {D}, divas saknes: x1 = {x1}, x2 = {x2}")
 elif D == 0:
     x = -b / (2*a)
-    print(fD = {D}, viena sakne: x = {x}")
+    print(f"D = {D}, viena sakne: x = {x}") #Iztrūkst pēdiņas
 else:
     print(f"D = {D}, nav reālu sakņu.")
